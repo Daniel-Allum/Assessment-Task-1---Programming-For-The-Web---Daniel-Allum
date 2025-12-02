376d9f08cfbec88fdd55911d599257a46f55a3f1
Daniel-Allum <daniel.allum@education.nsw.gov.au>
2025-12-02 03:46:14 +0000

    tables, users, reviews tables created

---
+++ DATABASE/Create_Tables.sql
@@ -0,0 +1,15 @@
+-- create table Movies(id INTEGER PRIMARY KEY AUTOINCREMENT,
+--                     title TEXT not null,
+--                     year INTEGER not null,
+--                     description TEXT not null);
+
+-- create table Users(id INTEGER PRIMARY KEY AUTOINCREMENT,
+--                     username TEXT not null,
+--                     password TEXT not null);
+
+-- create table Reviews(id INTEGER PRIMARY KEY AUTOINCREMENT,
+--                     Date DATE not null,
+--                     Review TEXT,
+--                     Rating INTEGER not null,
+--                     MovieID INTEGER not null,
+--                     UserID INTEGER not null);
